b1e6f3e32ebabf7c9d3476f6c8a820f6e87f13bc
Mohan Krishnan O <81806134+Mohankrish08@users.noreply.github.com>
2024-04-17 22:13:07 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,44 +1,19 @@
+import os
 import streamlit as st
-# from transformers import AutoTokenizer, AutoModelForCausalLM
-# import torch
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import HuggingFaceHub
 
-
-# model_name = "CohereForAI/c4ai-command-r-plus"
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModelForCausalLM.from_pretrained(model_name)
-
-# # Function to generate text
-# def generate_text(prompt, max_length=50, num_return_sequences=1, do_sample=True, top_k=50, top_p=0.95, num_beams=1, early_stopping=False):
-#     input_ids = tokenizer.encode(prompt, return_tensors="pt")
-#     output_ids = model.generate(
-#         input_ids,
-#         max_length=max_length,
-#         num_return_sequences=num_return_sequences,
-#         do_sample=do_sample,
-#         top_k=top_k,
-#         top_p=top_p,
-#         num_beams=num_beams,
-#         early_stopping=early_stopping,
-#     )
-#     generated_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-#     return generated_text
-
-
-def model_inference(texts):
-    model_name = "CohereForAI/c4ai-command-r-plus"
-    llm = HuggingFaceHub(repo_id = model_name, model_kwargs = {'temperature':0.2, 'max_length': 500}, huggingfacehub_api_token=<>)
-    return llm(texts)
-
-
-
-# Streamlit app
 st.title("Custom LLM Model")
-st.write("This app uses a pre-trained Coher model to generate text.")
+st.write("This app uses a pre-trained Mistral model to generate text.")
 
 prompt = st.text_area("Enter a prompt:", height=100)
-if st.button("Generate Text"):
-    generated_text = model_inference(prompt)
-    st.write("Generated text:", generated_text)
 
+if st.button("Generate Text"):
+    model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+    llm = HuggingFaceHub(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+        model_kwargs={"temperature": 0.3, "max_length": 1080},
+        huggingfacehub_api_token='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'
+    )
+    response = llm(prompt)
+    st.write("Generated text:", response.text)
